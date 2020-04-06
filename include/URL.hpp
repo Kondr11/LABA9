29e89028be9aad4780342acdce5f316758b96593
Kondr11 <alexk1201@gmail.com>
2020-04-06 18:07:38 +0400

    first try

--- include/URL.hpp
+++ include/URL.hpp
@@ -1,7 +1,7 @@
 // Copyright 2020 Kondr11
 
-#ifndef INCLUDE_URL_HPP
-#define INCLUDE_URL_HPP
+#ifndef INCLUDE_URL_HPP_
+#define INCLUDE_URL_HPP_
 
 #include <header.hpp>
 
@@ -14,16 +14,16 @@ struct URL{
         std::string s(url);
         std::regex regex(R"((https?:)\/\/([a-z.]*)\/?([a-z.\/]*))");
         std::smatch values;
-        if(std::regex_search(s, values, regex)){
-            if(values.size() > 1){
+        if (std::regex_search(s, values, regex)){
+            if (values.size() > 1){
                 protocol = values[1];
                 port = (protocol == "https:") ? "443" : "80";
-                if(values.size() > 2){
+                if (values.size() > 2){
                     host = values[2];
-                    if(values.size() > 3){
+                    if (values.size() > 3){
                         path = "/" + values[3].str();
                     }
-                    else{
+                    else {
                         path = "/";
                     }
                 }
@@ -32,4 +32,4 @@ struct URL{
     }
 };
 
-#endif //INCLUDE_URL_HPP
+#endif //INCLUDE_URL_HPP_
